8f46b2a072bae6b2d7db25031b6c533d6b35de0e
Fernando2004 <42586196+Fernando2004@users.noreply.github.com>
2019-10-26 21:51:32 +0200

    Add files via upload

---
+++ Leerarchivotexto.java
@@ -0,0 +1,44 @@
+package com.fernandogarcia.fichero;
+
+import java.io.*;
+
+public class Leerarchivotexto {
+
+    public static void main(String[] args) throws IOException {
+
+
+        File file =new File("./fichero.txt");
+
+        FileReader fr=null;
+        BufferedReader br=null;
+
+        String linea="";
+
+        StringBuilder stringBuilder = null;
+
+        try {
+            fr = new FileReader(file);
+            br= new BufferedReader(fr);
+
+            while ((linea = br.readLine()) != null){
+                stringBuilder = new StringBuilder(linea);
+                System.out.println(stringBuilder);
+
+            }
+        }
+        catch (FileNotFoundException fnf){
+            System.out.println("Archivo no encontrado ");
+
+        }
+        catch (IOException io){
+            System.out.println("error entrada/salida");
+
+        }
+        finally {
+            if(br!=null){
+                br.close();
+            }
+        }
+
+    }
+}
